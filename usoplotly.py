4afbcd0192ee6429c5dc1d30d9813ef07d4dfff7
jumair <54706910+jumair@users.noreply.github.com>
2022-07-20 17:15:05 +0200

    Add files via upload

---
+++ usoplotly.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import plotly.express as px
+
+# Abro el fichero donde tengo las actividades filtradas y correctas para representarlas de forma muy sencilla en un mapa
+ruta = 'D:/PythonDimas/JSON/'
+actividades = pd.read_csv(ruta + "actividades-100-fin.csv")
+
+# Preparo el mapa. Al ponerse encima de los puntos rojos aparece información (titulo, distrito, barrio, longitud y latitud) de la actividad.
+fig = px.scatter_mapbox(actividades, lat="Latitud", lon="Longitud", hover_name="Titulo", hover_data=["Distrito", "Barrio"],
+                        color_discrete_sequence=["red"], zoom=11, height=700)
+
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
